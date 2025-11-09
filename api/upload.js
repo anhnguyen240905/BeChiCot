78ae12c253963706d0101ffa51b13ad1e42ad8c3
anhnguyen240905 <k62.2315510021@ftu.edu.vn>
2025-11-10 05:37:49 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -7,12 +7,12 @@ export default async function handler(req, res) {
     const { data } = req.body; // base64 image
 
     // Lưu vào folder tạm trên server
-    const filePath = path.join(process.cwd(), "public", "temp-cert.png");
+    const filePath = path.join(process.cwd(), "public", "cert.png");
     const base64Data = data.replace(/^data:image\/png;base64,/, "");
     await writeFile(filePath, base64Data, "base64");
 
     // Trả về URL public
-    const url = `${req.headers.origin}/temp-cert.png`;
+    const url = `${req.headers.origin}/cert.png`;
     res.status(200).json({ url });
   } else {
     res.status(405).json({ error: "Method not allowed" });
