9fc6d677f473343259a335181612a480892954ac
anhnguyen240905 <k62.2315510021@ftu.edu.vn>
2025-11-10 08:08:36 +0700

    Create upload-cert.js

---
+++ pages/api/upload-cert.js
@@ -0,0 +1,28 @@
+// pages/api/upload-cert.js
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { imageBase64 } = req.body; // client gửi base64
+
+      const uploadResp = await cloudinary.uploader.upload(imageBase64, {
+        folder: "certs",
+      });
+
+      // trả về URL public
+      res.status(200).json({ url: uploadResp.secure_url });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Upload failed" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
